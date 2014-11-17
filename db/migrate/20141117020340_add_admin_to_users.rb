f17ea6ad89f87a7a19867d8fd63f66777827c183
mroach <mroach@mroach.com>
2014-11-17 11:26:26 +0800

    Added admin flag to users and unauthorized page (should have been part of a previous commit)

---
+++ db/migrate/20141117020340_add_admin_to_users.rb
@@ -0,0 +1,6 @@
+class AddAdminToUsers < ActiveRecord::Migration
+  def change
+    add_column :users, :admin, :boolean, default: false
+    User.find_by_username("mroach").update_attribute(:admin, true)
+  end
+end
