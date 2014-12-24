7a2f4f542edc3078c739a7e2f9d96bff9be7082f
mroach <mroach@mroach.com>
2014-12-24 16:54:10 +0400

    Allow users to update their names

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -42,10 +42,10 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.for(:sign_up) do |u|
-      u.permit(:first_name, :last_name, :username, :email, :password, :password_confirmation)
+      u.permit(:first_name, :last_name, :name, :username, :email, :password, :password_confirmation)
     end
     devise_parameter_sanitizer.for(:account_update) do |u|
-      u.permit(:first_name, :last_name, :username, :email, :password, :password_confirmation)
+      u.permit(:first_name, :last_name, :name, :username, :email, :password, :password_confirmation)
     end
   end
 
