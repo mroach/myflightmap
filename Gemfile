4a0d795ea77c78992dba85c140e1aed59c13df86
mroach <mroach@mroach.com>
2016-05-15 17:32:22 +0700

    Configure for Puma

--- Gemfile
+++ Gemfile
@@ -119,6 +119,7 @@ group :development do
   gem 'capistrano-bundler', '~> 1.1.2', require: false
   gem 'capistrano-rails', '~> 1.1.1', require: false
   gem 'capistrano-rbenv', '~> 2.0', require: false
+  gem 'capistrano3-puma',   require: false
 
   gem 'sqlite3'
 
--- config/deploy.rb
+++ config/deploy.rb
@@ -16,6 +16,19 @@ set :rollbar_token, secrets['rollbar']
 set :rollbar_env, Proc.new { fetch :stage }
 set :rollbar_role, Proc.new { :app }
 
+set :puma_threads, [4, 16]
+set :puma_workers, 0
+
+set :puma_bind,       "unix://#{shared_path}/tmp/sockets/#{fetch(:application)}-puma.sock"
+set :puma_state,      "#{shared_path}/tmp/pids/puma.state"
+set :puma_pid,        "#{shared_path}/tmp/pids/puma.pid"
+set :puma_access_log, "#{release_path}/log/puma.error.log"
+set :puma_error_log,  "#{release_path}/log/puma.access.log"
+set :ssh_options,     { forward_agent: true, user: fetch(:user), keys: %w(~/.ssh/id_rsa.pub) }
+set :puma_preload_app, true
+set :puma_worker_timeout, nil
+set :puma_init_active_record, true  # Change to false when not using ActiveRecord
+
 # Default branch is :master
 # ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }
 
@@ -41,11 +54,18 @@ set :default_env, { devise_secret_key: "herp", secret_key_base: "derp" }
 # set :keep_releases, 5
 
 namespace :deploy do
+  desc 'Initial Deploy'
+  task :initial do
+    on roles(:app) do
+      before 'deploy:restart', 'puma:start'
+      invoke 'deploy'
+    end
+  end
 
   desc 'Restart application'
   task :restart do
     on roles(:app), in: :sequence, wait: 5 do
-      execute :touch, release_path.join('tmp/restart.txt')
+      invoke 'puma:restart'
     end
   end
 
@@ -58,6 +78,10 @@ namespace :deploy do
     end
   end
 
+  before :starting,  :check_revision
+  after  :finishing, :compile_assets
+  after  :finishing, :cleanup
+  after  :finishing, :restart
 end
 
 namespace :figaro do
@@ -74,6 +98,18 @@ namespace :figaro do
       execute "ln -sf #{shared_path}/config/application.yml #{current_path}/config/application.yml"
     end
   end
+end
+
+namespace :puma do
+  desc 'Create Directories for Puma Pids and Socket'
+  task :make_dirs do
+    on roles(:app) do
+      execute "mkdir #{shared_path}/tmp/sockets -p"
+      execute "mkdir #{shared_path}/tmp/pids -p"
+    end
+  end
+
+  before :start, :make_dirs
 end
 
 after 'deploy:publishing', 'deploy:restart'
