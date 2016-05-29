232d961eb294a72545bf24aa2464e58df22cd0f9
mroach <mroach@mroach.com>
2016-05-29 17:13:31 +0700

    Update to Ruby 2.3.0

--- config/deploy.rb
+++ config/deploy.rb
@@ -14,7 +14,7 @@ set :log_level, :debug
 set :pty, true
 
 set :rbenv_type,     :user
-set :rbenv_ruby,     '2.1.5'
+set :rbenv_ruby,     File.read('.ruby-version').strip
 set :rbenv_prefix,   "RBENV_ROOT=#{fetch(:rbenv_path)} RBENV_VERSION=#{fetch(:rbenv_ruby)} #{fetch(:rbenv_path)}/bin/rbenv exec"
 set :rbenv_map_bins, %w{rake gem bundle ruby rails}
 set :rbenv_roles,    :all # default value
